ed1bdbd5f9b9025400f1d66feff23015f8f397dc
Maurice <mauriceglenn2@gmail.com>
2023-01-25 17:40:35 -0600

    Added login out features for buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import { auth } from './Firebase/init'
-import { React } from 'react'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import React from 'react'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+//onAuthStateChanged allows users to stay logged In when page is reloaded
 
 function App() {
   const [user, setUser] = React.useState({})
+  const [loading, setLoading] = React.useState(true)
+
+  React.useEffect(() =>{
+    onAuthStateChanged(auth, (user) =>{
+      setLoading(false)
+      console.log(user)
+        if (user) {
+          setUser(user)
+        }
+    })
+  }, []);
+  //run something when the page first mounts you need to pass an empty array in use effect
 
   function register() {
     console.log('Register')
@@ -15,23 +28,22 @@ function App() {
     .catch((error) => {
       console.log(error)
     })
-  
-    
   }
 
   function login() {
     signInWithEmailAndPassword(auth, "email@email.com", "password")
     .then(({ user }) => {
       console.log(user)
+      setUser(user)
     })
     .catch((error) => {
-     const error = seterrormessage("")
-      console.log(error)
+      console.log(error.message)
   })
   }
 
   function logout() {
-    signOut(auth)
+    signOut(auth);
+    setUser({});
 }
 
   return (
@@ -39,6 +51,7 @@ function App() {
 <button onClick={register}>Register</button>
 <button onClick={login}>Log In</button>
 <button onClick={logout}>Log Out</button>
+{loading ? "loading...." : user.email}
       </div>
   );
 
